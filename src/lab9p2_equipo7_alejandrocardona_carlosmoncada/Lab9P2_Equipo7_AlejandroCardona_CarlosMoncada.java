6cd3cebe8252277d4defdb33935644c2e27914dc
Alejandro Cardona <alexcardona3002@gmail.com>
2023-09-15 13:37:59 -0600

    Commit 1 - Creacion de Repositorio

---
+++ src/lab9p2_equipo7_alejandrocardona_carlosmoncada/Lab9P2_Equipo7_AlejandroCardona_CarlosMoncada.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab9p2_equipo7_alejandrocardona_carlosmoncada;
+
+/**
+ *
+ * @author jets
+ */
+public class Lab9P2_Equipo7_AlejandroCardona_CarlosMoncada {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
